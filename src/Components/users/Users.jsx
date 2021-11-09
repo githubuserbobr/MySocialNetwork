0e72101e5a8176752071bbb4a9f2263e096f0f1f
unknown <kozevnikovvalera@mail.ru>
2021-11-10 00:21:40 +0300

    class component 53

--- src/Components/users/Users.jsx
+++ src/Components/users/Users.jsx
@@ -2,8 +2,10 @@ import axios from "axios";
 import React from "react";
 import c from "./users.module.css";
 import userAva from "../../assets/images/users.jpg";
-const Users = (props) => {
-  if (props.users.length === 0) {
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
 
@@ -11,27 +13,31 @@ const Users = (props) => {
         props.setUsers(response.data.items);
       });
   }
-  return (
-    <div>
-      {props.users.map((u) => (
-        <div key={u.id} className={c.users}>
-          <div>{u.name}</div>
-          <img
-            src={u.photos.small != null ? u.photos.small : userAva}
-            className={c.img}
-          />
-          <div>{u.status}</div>
-          <div>
-            {" "}
-            {u.followed ? (
-              <button onClick={() => props.follow(u.id)}>FOLLOW</button>
-            ) : (
-              <button onClick={() => props.un_follow(u.id)}>UNFOLLOW</button>
-            )}
+  render() {
+    return (
+      <div>
+        {this.props.users.map((u) => (
+          <div key={u.id} className={c.users}>
+            <div>{u.name}</div>
+            <img
+              src={u.photos.small != null ? u.photos.small : userAva}
+              className={c.img}
+            />
+            <div>{u.status}</div>
+            <div>
+              {" "}
+              {u.followed ? (
+                <button onClick={() => this.props.follow(u.id)}>FOLLOW</button>
+              ) : (
+                <button onClick={() => this.props.un_follow(u.id)}>
+                  UNFOLLOW
+                </button>
+              )}
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+        ))}
+      </div>
+    );
+  }
+}
 export default Users;
